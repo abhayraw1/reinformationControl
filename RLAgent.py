3a9e91af0d2ba0c395eb292f77b196347a026a00
Abhay <abhayrawat2695@gmail.com>
2018-08-09 15:46:40 +0530

    major changes

--- RLAgent.py
+++ RLAgent.py
@@ -3,29 +3,43 @@ from PointEnvironment.Pose import Pose
 from PointEnvironment.Agent import Agent
 
 class RLAgent(Agent):
-  def __init__(self, edges, partial_env, train, pose=Pose(),  \
+  def __init__(self, id, train, pose=Pose(),  \
               defaultPose=False, collisionRadius=0.15, lenTrajectory=100):
     self.type     = "RLAgent"
+    self.id       = id
     assert type(edges) = dict
-    self.edges    = edges
-    self.env      = partial_env
-    self.n_nhbrs  = len(self.egdes)
-    super(RLAgent, self).__init__(agentpair)
-
-  def initEdgeControllers(self):
+    self.edges    = {}
+    self.train    = train
     self.edgeControllers = {}
-    for edge in self.edges.keys():
-      self.edgeControllers[edge] = DDPG("""Pass args here!!""")
+    super(RLAgent, self).__init__(id, pose=Pose(), defaultPose=False, \
+      collisionRadius=0.15, lenTrajectory=100)
+
+  def initEdgeControllers(self, agent_id, controller):
+    self.edgeControllers[agent_id] = controller
+
+  def addEdges(self, edges):
+    assert type(edges) == list
+    for edge in edges:
+      self.addEdge(edge)
+
+  def addEdge(self, edge):
+    assert isinstance(edge, Edge)
+    assert edge.i.id == self.id
+    self.edges[edge.j.id] == edge
+    self.n_nhbrs  = len(self.egdes)
 
   # training is done for only two agents at a time to ensure that averaging
   # velocities don't have any negative imact on training...
-  def act(self):
-    action = np.zeros(2, 'f').reshape((1,2))
-    for k in self.edgeControllers.keys():
-      obs = self.edges[k].state().reshape((1, 3))
-      action += controller.act(obs, train=self.train)
-    action = action/len(self.edges)
-    for k in self.edgeControllers.keys():
-      next_state = partial_env.step(action)
-      reward, done = partial_env.getReward()
-      controller.replaybuffer.add(obs, action, reward, next_state, done)
+
+
+  # def act(self):
+  #   action = np.zeros(2, 'f').reshape((1,2))
+  #   for k in self.edgeControllers.keys():
+  #     obs = self.edges[k].state().reshape((1, 3))
+  #     action += controller.act(obs, train=self.train)
+  #   action = action/len(self.edges)
+  #   for k in self.edgeControllers.keys():
+  #     next_state, reward, done = partial_env.step(action, k)
+  #     if self.train:
+  #       controller.replaybuffer.add(obs, action, reward, next_state, done)
+  #       controller.train()
