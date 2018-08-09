213185df0aa5bf2a5ae37d4e1041f15242ea6814
Abhay <abhayrawat2695@gmail.com>
2018-08-09 12:19:04 +0530

    Populated class

--- RLAgent.py
+++ RLAgent.py
@@ -1,17 +1,30 @@
 from Edge import AgentEdge
+from PointEnvironment.Pose import Pose
 
-class RLAgent(AgentEdge):
-  def __init__(self, id, agentpair, model):
-    self.id    =
-    self.model = model
+class RLAgent(Agent):
+  def __init__(self, edges, partial_env, train, pose=Pose(),  \
+              defaultPose=False, collisionRadius=0.15, lenTrajectory=100):
+    self.type     = "RLAgent"
+    assert type(edges) = dict
+    self.edges    = edges
+    self.env      = partial_env
+    self.n_nhbrs  = len(self.egdes)
     super(RLAgent, self).__init__(agentpair)
 
-  def act_and_train(self, obs):
-    # returns action performed by agent based on observation according to model
-    pass
+  def initEdgeControllers(self):
+    self.edgeControllers = {}
+    for edge in self.edges.keys():
+      self.edgeControllers[edge] = DDPG("""Pass args here!!""")
 
-  def act(self, obs):
-    pass
-
-  def stop_episode(self, obs):
-    pass
+  # training is done for only two agents at a time to ensure that averaging
+  # velocities don't have any negative imact on training...
+  def act(self):
+    action = np.zeros(2, 'f').reshape((1,2))
+    for k in self.edgeControllers.keys():
+      obs = self.edges[k].state().reshape((1, 3))
+      action += controller.act(obs, train=self.train)
+    action = action/len(self.edges)
+    for k in self.edgeControllers.keys():
+      next_state = partial_env.step(action)
+      reward, done = partial_env.getReward()
+      controller.replaybuffer.add(obs, action, reward, next_state, done)
