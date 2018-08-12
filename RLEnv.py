362a1e7b3771c7838e3c3572eb37bb23b40f54b4
Abhay <abhayrawat2695@gmail.com>
2018-08-12 11:25:42 +0530

    Reward function overhauled and some minor modifications

--- RLEnv.py
+++ RLEnv.py
@@ -1,19 +1,21 @@
+import numpy as np
 from PointEnvironment.Environment import PointEnvironment
 from Edge import Edge
+import HyperParams as HP
 from Shape import ShapeByAgents, Shape
 
 class FormationEnvironment(PointEnvironment):
   def __init__(self, targetshape, agents, num_iterations=100, dt=0.01):
     super(FormationEnvironment, self).__init__(num_iterations, dt, agents)
-    agentpairs = [(i, j) for i in self.agents \
-                      for j in self.agents if i.id != j.id]
+    self.agentpairs = [(i, j) for i in agents \
+                      for j in agents if i.id != j.id]
     assert isinstance(targetshape, Shape)
-    self.shape = ShapeByAgents(agentpairs)
+    self.shape = ShapeByAgents(self.agentpairs)
     self.edges = self.shape.edges
     self.targetshape = targetshape
     self._bindEdgesToAgents()
 
-  def stepAgent(self, action, agent_id):
+  def stepAgent(self, agent_id, action):
     action = np.matrix(action)
     self.step({agent_id: action})
     self.updateEdges()
@@ -22,8 +24,13 @@ class FormationEnvironment(PointEnvironment):
     for i in self.edges.values():
       i.update()
 
-  def reward(self):
-    return self.shape - targetshape
+  def cost(self, agent_id=None):
+    cost = self.shape - self.targetshape
+    # print "cost: ", cost
+    if agent_id != None:
+      print "||{}|| {}".format(agent_id, sum([cost[i, j] for (i,j) in cost.keys() if i == agent_id]))
+      return sum([cost[i, j] for (i,j) in cost.keys() if i == agent_id])
+    return cost
 
   def _bindEdgesToAgents(self):
     for i, j in self.edges.keys():
@@ -31,7 +38,7 @@ class FormationEnvironment(PointEnvironment):
 
 
 class AgentObservedEnvironment:
-  def __init__(self, world, agent)
+  def __init__(self, world, agent):
     self.agent    = agent
     self.agent_id = agent.id
     self.world    = world
@@ -41,18 +48,24 @@ class AgentObservedEnvironment:
     return _getNextState()
 
   def step(self, action):
-    self.world.stepAgent({self.id: action})
-    return _getNextState(), _getAgentReward(), isTerminal()
+    prev_cost = self._getCost()
+    self.world.stepAgent(self.agent_id, action)
+    # prev_states = {i.j.id:i.state() for i in self.agent.edges.values()}
+    reward = self._getAgentReward(prev_cost)
+    return self._getNextState(), reward, self._isTerminal()
 
   def _getNextState(self):
-    return {i:i.state() for i in self.agent.edges.values()}
+    return {i.j.id:i.state() for i in self.agent.edges.values()}
 
-  def _getAgentReward(self):
-    reward = 0
-    for k, v in world.reward().values():
-      if k == self.agent_id:
-        reward += v
-    return reward
+  def _getCost(self):
+    return self.world.cost(self.agent_id)
+
+
+  def _getAgentReward(self, prev_cost=None):
+    # print "total reward: {} [called from AOE for Agent:{}]".format(self.world.reward(), self.agent_id)
+    cost = self._getCost()
+    return (cost - prev_cost)*HP.REWARD_SCALE
+    # return sum([reward[i, j] for (i, j) in reward.keys() if i == self.agent_id])
 
   def _isTerminal(self):
-    return world.collisionOccured
+    return self.world.collisionOccured
