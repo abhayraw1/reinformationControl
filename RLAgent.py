22accd5915c6791194766b2570dd951ca0257bc2
Abhay <abhayrawat2695@gmail.com>
2018-08-12 11:24:53 +0530

    Minor bug fixes

--- RLAgent.py
+++ RLAgent.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Edge import Edge
 from PointEnvironment.Pose import Pose
 from PointEnvironment.Agent import Agent
 
@@ -7,12 +8,11 @@ class RLAgent(Agent):
               defaultPose=False, collisionRadius=0.15, lenTrajectory=100):
     self.type     = "RLAgent"
     self.id       = id
-    assert type(edges) = dict
     self.edges    = {}
     self.train    = train
     self.edgeControllers = {}
-    super(RLAgent, self).__init__(id, pose=Pose(), defaultPose=False, \
-      collisionRadius=0.15, lenTrajectory=100)
+    super(RLAgent, self).__init__(id, pose=pose, defaultPose=defaultPose, \
+      collisionRadius=collisionRadius, lenTrajectory=lenTrajectory)
 
   def initEdgeControllers(self, agent_id, controller):
     self.edgeControllers[agent_id] = controller
@@ -25,8 +25,8 @@ class RLAgent(Agent):
   def addEdge(self, edge):
     assert isinstance(edge, Edge)
     assert edge.i.id == self.id
-    self.edges[edge.j.id] == edge
-    self.n_nhbrs  = len(self.egdes)
+    self.edges[edge.j.id] = edge
+    self.n_nhbrs  = len(self.edges)
 
   # training is done for only two agents at a time to ensure that averaging
   # velocities don't have any negative imact on training...
