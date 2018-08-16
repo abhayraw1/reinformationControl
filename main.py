6bcca1f3c803182e394961951d1238c66c88e24a
Abhay <abhayrawat2695@gmail.com>
2018-08-16 12:59:59 +0530

    added evaluations

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import numpy as np
+from numpy import cos, sin
+from DDPG import DDPG
+import HyperParams as HP
 from RLAgent import RLAgent
 from Shape import ShapeByGeometry
-from FormationEnvironment import FormationEnvironment
+from PointEnvironment.Pose import Pose
+from RLEnv import FormationEnvironment, AgentObservedEnvironment
 
 ############### CONSTANTS ###############
 NUM_AGENTS = 2
@@ -10,24 +15,147 @@ NUM_AGENTS = 2
 ######## TARGET SHAPE DEFINITION ########
 geometry = {'coordinates': [[0,0], [2,0]],
             'orientations':[np.pi/2]*2 }
+geometry['coordinates'] = map(np.array, geometry['coordinates'])
 
 targetshape = ShapeByGeometry(geometry)
 #########################################
 
+def rnd(x = 5):
+  return np.random.random()*x-x/2.
+
+def randomtarget():
+  geometry = {'coordinates': [[rnd(),rnd()], [rnd(),rnd()]],
+              'orientations':[rnd(2*np.pi)]*2 }
+  geometry['coordinates'] = map(np.array, geometry['coordinates'])
+
+  return ShapeByGeometry(geometry)
 
 ############### AGENTS
-agents = [RLAgent(i, train=True) for i in range(2)]
+agents_pose = [Pose(0,0), Pose(0, -2, -np.pi/2)]
+agents = [RLAgent(i, pose=agents_pose[i]) for i in range(NUM_AGENTS)]
+eval_agents = [RLAgent(i, pose=agents_pose[i]) for i in range(NUM_AGENTS)]
 
 ############### ENVIRONMENT
 env = FormationEnvironment(targetshape, agents)
+eval_env = FormationEnvironment(targetshape, eval_agents)
 
 ############### PARTIALLY OBSERVED ENVS
 agent_observed_envs = {}
-for agent in env.agents:
-  agent_observed_envs[i.id] = AgentObservedEnvironment(env, agent)
+for agent in env.agents.values():
+  agent_observed_envs[agent.id] = AgentObservedEnvironment(env, agent)
 
-main_model = DDPG()
+eval_agent_observed_envs = {}
+for agent in eval_env.agents.values():
+  eval_agent_observed_envs[agent.id] = AgentObservedEnvironment(env, agent)
 
-for i, j in env.agents.keys():
-  controller = DDPG()
-  env.agents[i].initEdgeControllers(j, controller)
+############### MAIN TARGET MODEL
+main_model = DDPG(train=False)
+print "*"*80
+print "MAINMODEL: ", main_model
+############### INIT AGENT EDGES
+for i, j in env.agentpairs:
+  i.initEdgeControllers(j.id, DDPG(target_model=main_model))
+
+
+for i, j in eval_env.agentpairs:
+  i.initEdgeControllers(j.id, main_model)
+print "*"*80
+for i, j in env.agentpairs:
+  print i.edgeControllers[j.id].target_model
+
+for i, j in eval_env.agentpairs:
+  print i.edgeControllers[j.id].target_model
+
+############### GEN RANDOM POSE FOR AGENTS
+def randPose():
+  return Pose(*(np.random.random(3)*k - k/2.).tolist())
+############### SAVE MODEL
+def savemodel(eps):
+  main_model.actor.model.save('model/actor/E_{}.h5'.format(eps))
+  main_model.critic.model.save('model/critic/E_{}.h5'.format(eps))
+
+############### SAVE STATS
+def write_stats(x, y, file):
+  f = open(file, 'a+')
+  f.write('EPS: {}\nR {}\n\n'.format(x, y))
+  f.close()
+
+############### EVAL FUNCTION
+def evaluate(model, num_times, eps_len, eps):
+  trewards = []
+  for i in xrange(num_times):
+    print "EVAL RUN {}".format(i)
+    r = {i.id:0 for i in agents}
+    terminal = []
+    collision = False
+    eval_env.targetshape = randomtarget()
+    eval_env.reset({x.id:randPose() for x in agents})
+    for aoe in eval_agent_observed_envs.values():
+      aoe.reset()
+    for j in xrange(eps_len):
+      for agent_id, aoe in eval_agent_observed_envs.items():
+        for nbr, state in aoe.current_st.items():
+          action, next_state = aoe.step(nbr, state)
+          reward, done, reason = aoe.getAgentReward()
+          r[agent_id] += reward
+          if done and reason == 'c':
+            collision = True
+            break
+          terminal.append(done)
+        if collision:
+          break
+    trewards.append(sum(r.values()))
+    print "--REWARD: ", sum(r.values())
+  ttrr = np.mean(trewards)
+  print "EVAL MEAN SCORE: ", ttrr
+  write_stats(eps, ttrr, 'mean_scores')
+  return ttrr
+
+
+eps = 1
+done = False
+k = np.array([10, 10, np.pi])
+best_mean_score = 0
+while eps <= HP.NUM_EPS:
+  print "EPISODE : {}".format(eps)
+  t = 1
+  r = {i.id:0 for i in agents}
+  terminal = []
+  collision = False
+  env.targetshape = randomtarget()
+  env.reset({x.id:randPose() for x in agents})
+  for aoe in agent_observed_envs.values():
+    aoe.reset()
+  more_steps = int(np.random.random()*eps/20)
+  while t <= (HP.MAX_EPS_LEN + more_steps):
+    for agent_id, aoe in agent_observed_envs.items():
+      for nbr, state in aoe.current_st.items():
+        action, next_state = aoe.step(nbr, state)
+        reward, done, reason = aoe.getAgentReward()
+        r[agent_id] += reward
+        aoe.remember(nbr, [state, action, reward, next_state[nbr], done])
+        aoe.agent.train_edge_controller(nbr)
+        if done and reason == 'c':
+          collision = True
+          break
+        terminal.append(done)
+      if collision:
+        break
+      if np.array(terminal).all():
+        print "--------------------FULL FORMATION FORMED--------------------"
+        print env.targetshape
+        print env.shape
+    t += 1
+  write_stats(eps, r, 'scores')
+  print "REWARD: ", sum(r.values())
+  if (eps % HP.EVAL_INTERVAL) == 0:
+    eval_score = evaluate(main_model, HP.NUM_EVALS, HP.EVAL_EPS_LEN, eps)
+    if best_mean_score < eval_score:
+      savemodel(eps)
+      best_mean_score = eval_score
+
+  for aoe in agent_observed_envs.values():
+    for nbr in aoe.current_st.keys():
+      for epoch in range(HP.NUM_EPOCS):
+        aoe.agent.train_edge_controller(nbr)
+  eps += 1
