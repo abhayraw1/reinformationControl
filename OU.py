293e4f89d3d81b3fe8ecee8b14a3ac456b078631
Abhay <abhayrawat2695@gmail.com>
2018-08-16 12:57:42 +0530

    implementation improvement

--- OU.py
+++ OU.py
@@ -1,6 +1,11 @@
 import random
 import numpy as np
 
-class OU(object):
-  def __call__(self, x, mu, theta, sigma):
-    return theta * (mu - x) + sigma * np.random.randn(1)
+class OU:
+  def __init__(self):
+    pass
+
+  @staticmethod
+  def ou(x, mu, theta, sigma):
+    x = np.array(x)
+    return theta * (mu - x) + sigma * np.random.randn(x.shape[1])
